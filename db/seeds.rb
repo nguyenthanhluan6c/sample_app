b8ee3ee194860de7feab63240bb0a86415bc93e8
Nguyen Thanh Luan <nguyen.thanh.luan@framgia.com>
2016-02-24 11:23:41 +0700

    Add user microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,16 @@ User.create!(name:  "Example User",
              activated: true,
              activated_at: Time.zone.now)
 
+User.create!(name:  "Kratos",
+             email: "nguyenthanhluan6c@gmail.com",
+             password:              "111111",
+             password_confirmation: "111111",
+             admin:     true,
+             activated: true,
+             activated_at: Time.zone.now)
+
+
+
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
@@ -16,4 +26,11 @@ User.create!(name:  "Example User",
               password_confirmation: password,
               activated: true,
               activated_at: Time.zone.now)
+end
+
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
 end
