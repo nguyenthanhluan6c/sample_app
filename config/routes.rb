9a0cd7b67661b2797c3a04659fc8949203aa10d9
Nguyen Thanh Luan <nguyen.thanh.luan@framgia.com>
2016-02-25 09:28:07 +0700

    cm

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+  get    'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
@@ -12,7 +13,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :users
+
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
