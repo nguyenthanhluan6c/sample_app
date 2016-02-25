0c9704bde9e904d0a020a577acf05637b5bfb858
Nguyen Thanh Luan <nguyen.thanh.luan@framgia.com>
2016-02-25 10:34:35 +0700

    gmail mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   user_name:         =>    'nanofresh.vn@gmail.com',
   password:          =>    '12345678a@',
   authentication:    =>    'plain',
-  enable_starttls_auto: true  }
+  enable_starttls_auto:  =>  true  }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
