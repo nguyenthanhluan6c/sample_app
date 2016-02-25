5bff2f06cfaed2c05fb95a6641c39aaf1ee22be4
Nguyen Thanh Luan <nguyen.thanh.luan@framgia.com>
2016-02-25 10:01:38 +0700

    gmail mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,19 +63,28 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
+  #config.action_mailer.delivery_method = :smtp
+  #host = 'peaceful-savannah-78991.herokuapp.com'
+  #config.action_mailer.default_url_options = { host: host }
+  #ActionMailer::Base.smtp_settings = {
+   # :address        => 'smtp.sendgrid.net',
+   #:port           => '587',
+   # :authentication => :plain,
+  #  :user_name      => ENV['SENDGRID_USERNAME'],
+  #  :password       => ENV['SENDGRID_PASSWORD'],
+   # :domain         => 'heroku.com',
+   # :enable_starttls_auto => true
+  #}
+  #Gmail
   config.action_mailer.delivery_method = :smtp
-  host = 'peaceful-savannah-78991.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
-
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'google.com',
+  user_name:            'nanofresh.vn',
+  password:             '12345678a@',
+  authentication:       'plain',
+  enable_starttls_auto: true  }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
